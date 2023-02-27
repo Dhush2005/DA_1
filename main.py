d48b52fe49c32e9fbbe823564835b13cfecf1354
grobenthamjingxun <126137918+grobenthamjingxun@users.noreply.github.com>
2023-02-27 14:06:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ def makeCommits (days : int):
             file.write(f'{dates} <- this was the commit for the day!!\n')
 
         #staging
+                os.system('git add data.txt')
+
+        #commit
+        os.system('git commit --date="' + dates +'" -m "First commit for the day!"')
+
+        return days * makeCommits(days - 1)
+    makeCommits(15)
